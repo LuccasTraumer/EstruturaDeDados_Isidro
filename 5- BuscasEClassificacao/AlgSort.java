debd568a0da5055c65d0e19428cf7f85f3c3d357
LuccasTraumer <lucas.jeesus@hotmail.com>
2019-12-29 09:50:38 -0300

    Ideia do Quick Sort

--- 5- BuscasEClassificacao/AlgSort.java
+++ 5- BuscasEClassificacao/AlgSort.java
@@ -1,6 +1,8 @@
 public class AlgSort{
 	private int[] valores = {25,48,37,12,57,33,92};
 
+
+	public int getTamVet(){return this.valores.length;}
 	public void bubbleSort(){
 		int it;  // Indicador Lento, Vai marcar tambem a Itera√ß√£o em que esta
 		int pos; // Vai Indicar a Posicao Atual do vetor
@@ -52,12 +54,51 @@ public class AlgSort{
 		int indice,indiceAnterior,valorAtual;
 		for(indice = 1; indice < valores.length; indice++){
 			valorAtual = valores[indice];
-			for(indiceAnterior = indice-1; (indiceAnterior >= 0 && valores[indiceAnterior] > valorAtual); indiceAnterior--){
+			// nesse for a questao booleana È se o indiceAnt È menor ou Igual a Zero
+			// E se valor no IndiceAnterior È menor que o valor Atual,Quando encontrar um Valor menor ele vai parar de colocar os valores
+			// a direita, e anterior vai ser menor que o Valor atual e o prox vai ser maior que o Valor atual
+			// Ent„o vocÍ incrementa um no indice e coloca entre esses dois valores o valorAtual
+			for(indiceAnterior = indice-1; (indiceAnterior >= 0 && valores[indiceAnterior] > valorAtual); indiceAnterior--)
+			{
 				valores[indiceAnterior+1] = valores[indiceAnterior];
 			}
 			valores[indiceAnterior+1] = valorAtual;
 		}
 		mostrar();
+	}
+
+	public int patition(int inicio,int fim){
+		int temp,ref,up,down;
+		ref = valores[inicio];
+		down = inicio;
+		up = fim;
+		while(down < up){
+			while(valores[down] <= ref && down < up){
+				down++;
+			}
+			while(valores[up] > ref){
+				up--;
+			}
+			if(down < up){
+				temp = valores[down];
+				valores[down] = valores[up];
+				valores[up] = temp;
+			}
+		}
+		valores[inicio] = valores[up];
+		valores[up] = ref;
+		return up;
+	}
+	public void quickSort(int inicio, int fim){
+		int pivo = inicio;
+		if(inicio >= fim)
+			return;
+		else{
+			pivo = patition(inicio,fim);
+			quickSort(inicio, pivo-1);
+			quickSort(pivo+1,fim);
+		}
+
 	}
 	public void mostrar(){
 		for(int i=0; i < valores.length;i++)
--- 5- BuscasEClassificacao/MyAppSort.java
+++ 5- BuscasEClassificacao/MyAppSort.java
@@ -2,7 +2,8 @@ public class MyAppSort{
 	public static void main(String[] args){
 		AlgSort tes = new AlgSort();
 
-		tes.insertionSort();
+		tes.quickSort(0,tes.getTamVet()-1);
+		tes.mostrar();
 
 	}
 }
